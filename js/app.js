a51cb5d1f1d4d52e3e270702bee7093ab3b3a424
Luis Rabines <lurabines@gmail.com>
2019-07-23 16:36:29 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,72 @@
+/*
+ * Create a list that holds all of your cards
+ */
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+const cardsList = [
+	'fa-diamond',
+	'fa-diamond',
+	'fa-paper-plane-o',
+	'fa-paper-plane-o',
+	'fa-anchor',
+	'fa-anchor',
+	'fa-bolt',
+	'fa-bolt',
+	'fa-cube',
+	'fa-cube',
+	'fa-leaf',
+	'fa-leaf',
+	'fa-bomb',
+	'fa-bomb',
+	'fa-bicycle',
+	'fa-bicycle'
+];
+
+const cardDeck = document.querySelector('ul.deck');
+let openCards = [];
+
+cardsList.forEach(function(card) {
+	const li = document.createElement('li');
+	li.className = 'card';
+	li.innerHTML = `<i class="fa ${card}"></i>`;
+	cardDeck.appendChild(li);
+
+	li.addEventListener('click', function clickCard(cardElement) {
+		const cardTarget = cardElement.target;
+		if (openCards.length <= 1) {
+			cardTarget.classList.add('open', 'show');
+			openCards.push(cardTarget.innerHTML);
+		}
+	});
+});
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
