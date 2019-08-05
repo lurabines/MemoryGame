45d030de3e13ddcc4319b5fb560d11569502e369
Luis Rabines <luisrabines51@gmail.com>
2019-08-05 11:03:30 -0400

    added the start and the stop timer functions.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
+/*
+ * Create a list that holds all of your cards
+ */
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
 const deckOfCards = document.querySelector('ul.deck');
-const cardsList = [
-	'fa-diamond',
+let cardsList = [
 	'fa-diamond',
 	'fa-paper-plane-o',
-	'fa-paper-plane-o',
-	'fa-anchor',
 	'fa-anchor',
 	'fa-bolt',
-	'fa-bolt',
-	'fa-cube',
 	'fa-cube',
 	'fa-leaf',
-	'fa-leaf',
 	'fa-bomb',
-	'fa-bomb',
-	'fa-bicycle',
 	'fa-bicycle'
 ];
+cardsList = cardsList.concat(cardsList);
+const stars = document.querySelectorAll('.stars li');
+let rating = 3;
+const minutesText = document.querySelector('#timer .minutes');
+const secondsText = document.querySelector('#timer .seconds');
 const restartBtn = document.querySelector('.restart');
 const moveContainer = document.querySelector('.moves');
+moveContainer.innerHTML = 0;
 let moves = 0;
 let openCards = [];
 let matchCards = 0;
@@ -33,6 +51,24 @@ restartBtn.addEventListener('click', restartGame);
 //Restart The Game function
 function restartGame() {
 	console.log('restart button');
+	location.reload(true);
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
 
 //Create the Card Deck and put it in the DOM
@@ -43,11 +79,52 @@ function createDeck() {
 		li.innerHTML = `<i class="fa ${card}"></i>`;
 		document.querySelector('ul.deck').appendChild(li);
 	});
+	startTimer();
 }
-
+//Start Counting the User moves
 function countMoves() {
 	moves++;
 	moveContainer.textContent = moves;
+	starRating();
+}
+
+//Star Rating in the Game
+function starRating() {
+	if (moves === 17) {
+		rating--;
+		stars[2].classList.add('empty-star');
+		console.log(rating);
+	} else if (moves === 26) {
+		rating--;
+		stars[1].classList.add('empty-star');
+		console.log(rating);
+	} else if (moves === 34) {
+		rating--;
+		stars[0].classList.add('empty-star');
+		console.log(rating);
+	}
+}
+
+//Start Timer for the Game
+function startTimer() {
+	seconds = 0;
+	minutes = 0;
+
+	//The Timer for the Game
+	function setTimer() {
+		seconds = seconds + 1;
+		if (seconds > 59) {
+			seconds = 00;
+			minutes = minutes + 1;
+		}
+		secondsText.innerText = seconds;
+		minutesText.innerText = minutes;
+	}
+	setInterval(setTimer, 1000);
+}
+//Stop Timer for the Game
+function stopTimer() {
+	clearInterval(startTimer);
 }
 
 //Add and Event Listener to the Card Deck, and listen to the user click target.
@@ -90,6 +167,7 @@ function cardsMatch() {
 	matchCards += 2;
 	if (matchCards === 16) {
 		setTimeout(gameComplete, 400);
+		stopTimer();
 	}
 	openCards = [];
 	enableListeners();
@@ -104,21 +182,4 @@ function cardsDontMatch() {
 
 function gameComplete() {
 	console.log('Game complete, all cards have been matched!');
-}
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-	var currentIndex = array.length,
-		temporaryValue,
-		randomIndex;
-
-	while (currentIndex !== 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-
-	return array;
 }
